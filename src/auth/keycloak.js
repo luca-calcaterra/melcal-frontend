791633a5a44d0f6f72af8d4e704b2599795e00b7
Luca Calcaterra <luca.calcaterra@keypartner.com>
2026-02-16 17:19:42 +0100

    new keycloak endpoint

--- src/auth/keycloak.js
+++ src/auth/keycloak.js
@@ -1,7 +1,7 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-  url: "https://keycloak-app--0000001.ambitioussky-de3f00f7.eastus.azurecontainerapps.io/",
+  url: "https://keycloak-app--0000002.ambitioussky-de3f00f7.eastus.azurecontainerapps.io/",
   realm: "melcal",
   clientId: "react-frontend",
 });
